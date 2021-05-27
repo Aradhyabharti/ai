8a1e6b716db32a3db271620a197aad3d21ea64e4
Aradhyabharti <77610289+Aradhyabharti@users.noreply.github.com>
2021-05-27 13:28:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+ video = ""
+ status=""
+function preLoad(){
+    video = createVideo('video.mp4')
+    video.hide()
+}
+
+
+
+function setup(){
+    canvas=createCanvas(400,300)
+    canvas.center()
+}
+
+
+function draw(){
+    image(video,0,0,400,300)
+}
+
+function start(){
+    od=ml5.objectDetector('cocossd',modelLoaded)
+    document.getElementById("status").innerHTML="Detecting objects "
+}
+
+
+function modelLoaded(){
+    console.log('model is loaded')
+   status=true
+   video.loop()
+   video.speed(1)
+   video.volume(0)
+}
